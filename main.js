662d329328e73de9423498e95ff93953460c2a51
Jessica Kerr <jessitron@honeycomb.io>
2023-04-04 11:36:44 -0500

    context.with() doesn't work with an external span?

--- main.js
+++ main.js
@@ -33,8 +33,23 @@ sdk.start();
 main();
 sdk.shutdown();
 function main() {
+    const externalSpanContext = {
+        traceId: "39aec76ccd07ff96c2335c6d2b7c0048",
+        spanId: "b01dfd393fb17ee9",
+        isRemote: true,
+        traceFlags: 1, // this says that it is sampled: we do want to emit this trace
+    };
+    console.log("Is it a valid span context? " +
+        otel.trace.isSpanContextValid(externalSpanContext));
+    const newContext = otel.trace.setSpanContext(otel.context.active(), externalSpanContext);
     const tracer = otel.trace.getTracer("inside-node-program");
-    const s = tracer.startSpan("bananas");
-    console.log("span in trace " + s.spanContext().traceId);
-    s.end();
+    const realSpan = tracer.startSpan("ppo", {}, newContext);
+    const newContext2 = otel.trace.setSpanContext(otel.context.active(), realSpan.spanContext());
+    realSpan.end();
+    otel.context.with(newContext2, () => {
+        console.log("active context: " + JSON.stringify(otel.context.active()));
+        const s = tracer.startSpan("bananas", {});
+        console.log("span in trace " + s.spanContext().traceId);
+        s.end();
+    });
 }
--- main.ts
+++ main.ts
@@ -14,11 +14,37 @@ main();
 sdk.shutdown();
 
 function main() {
+  const externalSpanContext: otel.SpanContext = {
+    traceId: "39aec76ccd07ff96c2335c6d2b7c0048",
+    spanId: "b01dfd393fb17ee9",
+    isRemote: true,
+    traceFlags: 1, // this says that it is sampled: we do want to emit this trace
+  };
+  console.log(
+    "Is it a valid span context? " +
+      otel.trace.isSpanContextValid(externalSpanContext)
+  );
+
+  const newContext = otel.trace.setSpanContext(
+    otel.context.active(),
+    externalSpanContext
+  );
   const tracer = otel.trace.getTracer("inside-node-program");
 
-  const s = tracer.startSpan("bananas");
+  const realSpan = tracer.startSpan("ppo", {}, newContext);
+  const newContext2 = otel.trace.setSpanContext(
+    otel.context.active(),
+    realSpan.spanContext()
+  );
+  realSpan.end();
 
-  console.log("span in trace " + s.spanContext().traceId);
+  otel.context.with(newContext2, () => {
+    console.log("active context: " + JSON.stringify(otel.context.active()));
 
-  s.end();
+    const s = tracer.startSpan("bananas", {});
+
+    console.log("span in trace " + s.spanContext().traceId);
+
+    s.end();
+  });
 }
