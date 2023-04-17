3afbe950a6512ea065c1cfa4f9fcac36ef93b097
Jessica Kerr <jessitron@honeycomb.io>
2023-04-17 12:02:17 -0500

    works to send to hny
    next i need to parse traceparent

--- cli+node.sh
+++ cli+node.sh
@@ -8,50 +8,8 @@
 set -e
 set -x
 
-export OTEL_EXPORTER_OTLP_ENDPOINT=localhost:4317
+export OTEL_EXPORTER_OTLP_ENDPOINT=https://api.honeycomb.io:443
+export OTEL_EXPORTER_OTLP_HEADERS="x-honeycomb-team=${HONEYCOMB_API_KEY}"
 
-carrier=$(mktemp)    # traceparent propagation via tempfile
-sockdir=$(mktemp -d) # a unix socket will be created here
+otel-cli exec --service outside-service --name "script $0" npm start
 
-# start the span background server, set up trace propagation, and
-# time out after 30 seconds (which shouldn't be reached)
-otel-cli span background \
-    --tp-carrier $carrier \
-    --sockdir $sockdir \
-    --tp-print \
-    --service "jaswanthm-restful-booker-platform" \
-    --name "$0 script execution" \
-    --timeout 30 &
-
-data1=$(uuidgen)
-
-# add an event to the span running in the background, with an attribute
-# set to the uuid we just generated
-otel-cli span event \
-    --name "did a thing" \
-    --sockdir $sockdir \
-    --attrs "data1=$data1"
-
-# waste some time
-sleep 1
-
-# add an event that says we wasted some time
-otel-cli span event --name "slept 1 second" --sockdir $sockdir
-
-traceId=$(echo $(cut -d ":" -f2- <<< $(sed '1q;d' $carrier)))
-nospaces=${traceId## } # remove leading spaces
-nospaces=${traceId%% } # remove trailing spaces
-echo $traceId
-export TRACE_ID=$traceId
-
-spanId=$(echo $(cut -d ":" -f2- <<< $(sed '2q;d' $carrier)))
-nospaces=${spanId## } # remove leading spaces
-nospaces=${spanId%% } # remove trailing spaces
-echo $spanId
-export SPAN_ID=$spanId
-
-npm install
-npm run start
-
-# finally, tell the background server we're all done and it can exit
-otel-cli span end --sockdir $sockdir
--- main.ts
+++ main.ts
@@ -1,14 +1,27 @@
 import { NodeSDK } from "@opentelemetry/sdk-node";
 import * as otel from "@opentelemetry/api";
-import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
-
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-grpc";
+import { Metadata } from "@grpc/grpc-js";
 const { ConsoleSpanExporter } = require("@opentelemetry/sdk-trace-base");
 
+import { diag, DiagConsoleLogger, DiagLogLevel } from "@opentelemetry/api";
 
+// For troubleshooting, set the log level to DiagLogLevel.DEBUG
+diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
 
+console.log("What is the traceparent env var? " + process.env["TRACEPARENT"]);
+
+const metadata = new Metadata();
+metadata.set(
+  "x-honeycomb-team",
+  process.env["HONEYCOMB_API_KEY"] || "lack of api key"
+);
 const sdk = new NodeSDK({
-  traceExporter: new OTLPTraceExporter(),
-  // serviceName: "jaswanthm-restful-booker-platform"
+  traceExporter: new OTLPTraceExporter({
+    url: "https://api.honeycomb.io:443",
+    metadata,
+  }),
+  serviceName: "jaswanthm-restful-booker-platform",
 });
 
 sdk.start();
@@ -18,12 +31,11 @@ main();
 sdk.shutdown();
 
 function main() {
+  let traceId: string = process.env.TRACE_ID as string;
+  console.log("ENV Trace = " + traceId);
 
-  let traceId:string = process.env.TRACE_ID as string;
-  console.log("ENV Trace = " + traceId)
-
-  let spanId:string = process.env.SPAN_ID as string;
-  console.log("ENV Span = " + spanId)
+  let spanId: string = process.env.SPAN_ID as string;
+  console.log("ENV Span = " + spanId);
 
   const externalSpanContext: otel.SpanContext = {
     traceId: traceId,
@@ -43,7 +55,7 @@ function main() {
   const tracer = otel.trace.getTracer("jaswanthm-restful-booker-platform");
 
   otel.context.with(newContext, () => {
-    const s = tracer.startSpan("bananas", {});
+    const s = tracer.startSpan("bananas", { kind: otel.SpanKind.SERVER });
 
     console.log("span in trace " + s.spanContext().traceId);
 
